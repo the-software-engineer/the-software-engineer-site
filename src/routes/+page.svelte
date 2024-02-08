bf48ab09207e9d6e28eecf8f1546fa6466bb5145
Theo Gregory <theo@gregory.sh>
2024-02-08 11:21:16 +0200

    feat: simplified page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,11 +8,7 @@
 />
 
 <main class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8 mt-10">
-  <h1 class="text-3xl font-bold mb-6">
-    Welcome to Svelte GitHub Pages Quickstart
-  </h1>
-  <p>
-    Quickstart project to develop and deploy a static Svelte page to GitHub
-    pages.
-  </p>
+  <h1 class="text-3xl font-bold mb-6">The Software Engineer Ltd.</h1>
+  <p>Hired dev for your next idea.</p>
+  <ul><li><a href="https://getvocal.chat">Vocal</a></li></ul>
 </main>
