9e00d406cc3a367a7ad9e2034a89fdcf66757081
Theo Gregory <theo@gregory.sh>
2024-02-08 11:23:43 +0200

    feat: coloured vocal link

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,5 +10,7 @@
 <main class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8 mt-10">
   <h1 class="text-3xl font-bold mb-6">The Software Engineer Ltd.</h1>
   <p>Hired dev for your next idea.</p>
-  <ul><li><a href="https://getvocal.chat">Vocal</a></li></ul>
+  <ul>
+    <li><a class="text-orange-600" href="https://getvocal.chat">Vocal</a></li>
+  </ul>
 </main>
