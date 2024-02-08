deece64363ad07a9058070701d6b00742aac7471
Theo Gregory <theo@gregory.sh>
2024-02-08 11:34:26 +0200

    style: lint

--- e2e/test.spec.ts
+++ e2e/test.spec.ts
@@ -2,8 +2,6 @@ import { expect, test } from '@playwright/test';
 
 test('index page has expected h1', async ({ page }) => {
   await page.goto('/');
-  expect(await page.textContent('h1')).toContain(
-    'The Software Engineer Ltd.',
-  );
+  expect(await page.textContent('h1')).toContain('The Software Engineer Ltd.');
   expect(await page.getAttribute('a', 'href')).toBe('https://getvocal.chat');
 });
