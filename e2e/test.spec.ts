b355fe161fa6fcfeba598f35701f3733b2c191dd
Theo Gregory <theo@gregory.sh>
2024-02-08 11:26:28 +0200

    test: fix test

--- e2e/test.spec.ts
+++ e2e/test.spec.ts
@@ -3,6 +3,7 @@ import { expect, test } from '@playwright/test';
 test('index page has expected h1', async ({ page }) => {
   await page.goto('/');
   expect(await page.textContent('h1')).toContain(
-    'Welcome to Svelte GitHub Pages Quickstart',
+    'The Software Engineer Ltd.',
   );
+  expect(await page.getAttribute('a', 'href')).toBe('https://getvocal.chat');
 });
